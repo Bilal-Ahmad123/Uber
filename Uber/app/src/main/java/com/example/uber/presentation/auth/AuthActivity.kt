dcfb5cf77453f1a643e22fe68b34664da3a7a442
Bilal Ahmad <ba269953@gmail.com>
2025-02-03 23:47:37 +0500

    status bar fix

--- Uber/app/src/main/java/com/example/uber/presentation/auth/AuthActivity.kt
+++ Uber/app/src/main/java/com/example/uber/presentation/auth/AuthActivity.kt
@@ -1,6 +1,9 @@
 package com.example.uber.presentation.auth
 
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,16 +11,13 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.uber.R
 import dagger.hilt.android.AndroidEntryPoint
 
+
 @AndroidEntryPoint
 class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_auth)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
     }
 }
